4d141f94e80539f095528997024e377625231921
edulis8 <eric_broberg@hotmail.com>
2015-10-24 22:02:15 -0700

    playing with insta feeds

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
 
+
+
+$('.jumbotron').click(function(event) {
+	$(".insta a").attr("href", "https://instagram.com/edulis8/");
+	$(".insta a").attr("target", "_self");
+	$(".insta a").html("<p>hi</p>");
+	
+});
+
+	
+
+	$(".buy").attr("href", "edulisgardens.com")
+
+
+
+	$('.insta img').mouseover(function(event) {
+		/* Act on the event */
+
+
+		$('this').fadeOut('slow');
+
+		event.preventDefault();
+		window.open("https://instagram.com/edulis8/")
+	});
+});
