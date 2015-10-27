640408330cd12c5499bb7fbaf02edfa8c93f0f8e
edulis8 <eric_broberg@hotmail.com>
2015-10-26 21:07:59 -0700

    sidebar, media queries, some JS to randomize h2

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	var random = Math.floor((Math.random()*5) + 1);
+
+	console.log(random);
+
+	switch(random){
+		case 1:
+			$('.random').html("a patch of edible rarities")
+		break;
+		case 2:
+			$('.random').html("a stacked mosaic of nutritious exotics")
+		break;
+		case 3:
+			$('.random').html("a grove of unexpected specialties")
+		break;
+		case 4: 
+			$('.random').html("a sandy, subtropical agroforest")
+		break;
+		case 5:
+			$('.random').html("uncommon, unexpected specialty produce ")
+		break;
+		default:
+			$('.random').html("a patch of edible rarities")
+	}
+	
+});
